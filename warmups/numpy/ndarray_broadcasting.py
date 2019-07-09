5b9abf603443ade11bdd11b9df1bbb4a13983e87
ravi.sankar <ravi.sankar@altiux.com>
2019-07-09 20:31:28 +0530

    broadcasting

---
+++ warmups/numpy/ndarray_broadcasting.py
@@ -0,0 +1,21 @@
+import numpy as np
+#broadcasting is useful when working with two matrices with mismatching sizes. 
+start = np.zeros((4,3))
+print(start)
+
+add_rows = np.array([[1,0,2]])
+print(add_rows)
+y = start + add_rows
+print(y)
+
+add_cols = np.array([[0,1,2,3]])
+add_cols = add_cols.T #Transpose operation to convert 1X4 array to 4X1 array
+print(add_cols)
+
+y=start + add_cols
+print(y)
+
+#broadcasts on both direction
+ones = np.array([1])
+y=start+ones
+print(y)
