cd0581fd908ed6ec719e612a2c8c8c03517fcae7
r-ravisankar <ravisankar.r93@gmail.com>
2019-09-10 21:55:38 +0530

    committed latest

--- python/data_science/analytics/data_configuration/configure-tagmeta.py
+++ python/data_science/analytics/data_configuration/configure-tagmeta.py
@@ -13,15 +13,16 @@ unitId = "5c4ed3f5fe02914642b4d5bc"
 units = pd.read_csv("D:\work\exactspace\scripts\csv scripts\Renusagar_New_Meta_Unique.csv")
 units = pd.DataFrame(units)
 
-dataTags = units.dataTagId
 # print(dataTags)
 for i in range(0, units.shape[0]):
     urli = cfg +'/tagmeta/?filter={"where":{"dataTagId":"'+units.loc[i,'dataTagId']+'"}}'
     db_rows = requests.get(urli)
     if(db_rows.status_code == 200):
+        # print(db_rows.content)
         db_rows = json.loads(db_rows.content)
         csv_row = units.loc[i]
-        equipmentName = units.loc[i,'equipmentName']
+        # print(units.loc[i,'dataTagId'])
+        # equipmentName = units.loc[i,'equipmentName']
         data = {
         "equipmentName": units.loc[i,'equipmentName'],
         "equipment": units.loc[i,'equipment'],
@@ -49,7 +50,37 @@ for i in range(0, units.shape[0]):
         "systemName": units.loc[i,'systemName'],
         "unitsId": unitId
         }
-        print(i)
+
+        if(len(db_rows) > 1):
+            print()
+            # print('--> ',db_rows)
+            # print(db_rows['equipmentName'])
+            eqn_list=[]
+            for m in range(0,len(db_rows)):
+                eqn_list.append(db_rows[m].get('equipmentName'))
+            print(eqn_list)
+            for n in range(0,len(db_rows)):
+                if(db_rows[n].get('equipmentName') == units.loc[i,'equipmentName']):
+                    # update
+                    data["equipmentId"] = db_rows[n].get('equipmentId')
+                    update_url = cfg + '/tagmeta/update?where={"dataTagId" : "' +db_rows[n].get('dataTagId')+'"}'
+                    # print('update--- ',data)
+                    print()
+                    # update_req = requests.post(update_url, data=data)
+
+                else:
+                    # Add
+                    eq_url = cfg + '/equipment?filter={"where":{"unitsId": "' + unitId + '", "name": "'+units.loc[i,'equipmentName']+'"}}'
+                    eq_json = requests.get(eq_url)
+                    if(eq_json.status_code == 200):
+                        eq_json = json.loads(eq_json.content)
+                        eq_Id=eq_json[0].get('id')
+                        data['equipmentId'] = eq_Id
+                        post_url = cfg + '/equipment/'+eq_Id+'/tagmeta'
+                        # print('add -- ',data)
+                        # post_req = requests.post(post_url, data=data)
+        
+        # print(i)
         # CASE-3: If tag doesn't exist [if db_rows == []]
         if(db_rows==[]):
             # Add
@@ -61,8 +92,12 @@ for i in range(0, units.shape[0]):
                 data['equipmentId'] = eq_Id
                 post_url = cfg + '/equipment/'+eq_Id+'/tagmeta'
                 post_req = requests.post(post_url, data=data)
+        
+        # elif(len(db_rows)>1):
+        #     for i in range(0,len(db_rows)):
+        #         print(db_rows[i].get('equipmentName'))
 
-        # Case 2: If equipmentName in csv and db did not match. then fetch the eqname from the db
+        # Case 2: If equipmentName in csv and db did not match. then fetch the eqid from the db
         elif( db_rows[0].get('equipmentName') != units.loc[i,'equipmentName']):
             # Update the equipment ID
             eq_url = cfg + '/equipment?filter={"where":{"unitsId": "' + unitId + '", "name": "'+units.loc[i,'equipmentName']+'"}}'
