43867081f1ad452e2bd7ea4db368ca594c70f50e
r-ravi <ravisankar.r93@gmail.com>
2019-11-13 07:43:03 +0530

    restructured

--- python/data_science/analytics/data_cleaning_and_validations/boilertags_taglist.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv("BoilerTags_July31_Validation.csv")
-subset = pd.DataFrame(df)
-isEmpty = pd.isnull(subset['invalid'])
-validsub = subset[isEmpty]
-
-validsub.to_csv("BoilerTags_July31_Taglist.csv")
