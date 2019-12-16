ca9510ea627eb7576e364456b728ac7ed920716e
Ravi-Rsankar <18555421+Ravi-Rsankar@users.noreply.github.com>
2019-12-16 12:29:10 +0530

    remove on message for publisher

--- python/mqtt/publisher.py
+++ python/mqtt/publisher.py
@@ -2,27 +2,11 @@ import paho.mqtt.client as mqtt
 import time
 import json
 
-############
-def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
-
-def on_connect(client, obj, flags, rc):
-    print("connect: " + str(rc))
-
-def on_log(client, userdata, obj, buff):
-    pass
-    #print("log: " + str(buff))
-
-
 broker_address="172.31.1.164"
 #broker_address="iot.eclipse.org"
 print("creating new instance")
 client = mqtt.Client("P2") #create new instance
 
-client.on_message=on_message #attach function to callback
 print("connecting to broker")
 
 client.connect(broker_address) #connect to broker
