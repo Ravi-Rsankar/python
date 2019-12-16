36098243e5feb48705cb2fef037e2bf871cf0da0
Ravi-Rsankar <18555421+Ravi-Rsankar@users.noreply.github.com>
2019-12-16 12:29:55 +0530

    modified data

--- python/mqtt/publisher.py
+++ python/mqtt/publisher.py
@@ -13,13 +13,7 @@ client.connect(broker_address) #connect to broker
 
 client.loop_start() #start the loop
 query = {
-    "vars" : ["MAY91CX210","PI_06106"],
-    "start_value":1570390090000-60*60000,
-    "end_value":1570390090000,
-    "sample_value": 1,
-    "sample_unit": "hours",
-    "round":3,
-    "type":"object"
+    "data" : ["abc","123"]
 }
 
 print("Publishing message to topic","viz/request")
