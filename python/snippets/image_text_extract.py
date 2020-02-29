e7639e523f6e2cc34f8d66b917a8e02747190bb5
r-ravi <ravisankar.r93@gmail.com>
2020-02-29 09:02:36 +0530

    text extractor from images

---
+++ python/snippets/image_text_extract.py
@@ -0,0 +1,17 @@
+import cv2
+import pytesseract
+
+# This is the default location of tesseract when installed in windows
+pytesseract.pytesseract.tesseract_cmd = 'C:/Users/admbhed/AppData/Local/Tesseract-OCR/tesseract.exe'
+
+img = cv2.imread('download.png')
+text = pytesseract.image_to_string(img)
+print(text)
+
+with open('log.txt', 'w') as f:
+	f.write(text)
+
+import pandas as pd
+
+df = pd.read_fwf('log.txt')
+df.to_csv('log.csv')
